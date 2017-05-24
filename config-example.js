399867bbd2a67ac184f2efb87665df613c36a71b
pmagnaghi <pmagnagh@cisco.com>
2017-05-24 05:07:56 -0700

    changed to oAuth token
    for slack / cto integration

--- config-example.js
+++ config-example.js
@@ -2,7 +2,7 @@ var slackbot = require('./lib/bot');
 
 var config = {
     bot_name: "WebExCTO",//Provide the name to post under.  Default is WebexBo
-    token: 'xoxp-2535239080-2535280320-19399096774-13036fbf06',
+    token: 'xoxp-2535239080-2535280320-187280725122-70e3fd8a12d8142a9cc2875a10da0144',
     webex_urls: {
       "launch": "https://go.webex.com/meet/",//REPLACE THESE W/ YOUR COMPANY URLs
       "join": "https://go.webex.com/join/",
